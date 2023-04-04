e0337b99eda5cc89b5f6d4c61aaf6ff6f42006fe
fort nite <fornite@example.com>
2023-04-04 20:53:36 +0200

    Buttons and stuff

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,46 @@ export default function App() {
       setColorScheme('light');
     }
   };
+  const openYoutubePopup = () => {
+    const width = Math.round(window.innerWidth * 0.9);
+    const height = Math.round(window.innerHeight * 0.9);
+    const left = Math.round((window.innerWidth - width) / 2);
+    const top = Math.round((window.innerHeight - height) / 2);
 
+    window.open('https://www.youtube.com/', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
+  };
+  const openNetflixPopup = () => {
+    const width = Math.round(window.innerWidth * 0.9);
+    const height = Math.round(window.innerHeight * 0.9);
+    const left = Math.round((window.innerWidth - width) / 2);
+    const top = Math.round((window.innerHeight - height) / 2);
+
+    window.open('https://www.netflix.com/', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
+  };
+  const openHbomaxPopup = () => {
+    const width = Math.round(window.innerWidth * 0.9);
+    const height = Math.round(window.innerHeight * 0.9);
+    const left = Math.round((window.innerWidth - width) / 2);
+    const top = Math.round((window.innerHeight - height) / 2);
+
+    window.open('https://www.hbomax.com/', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
+  };  
+  const openDisneyplusPopup = () => {
+    const width = Math.round(window.innerWidth * 0.9);
+    const height = Math.round(window.innerHeight * 0.9);
+    const left = Math.round((window.innerWidth - width) / 2);
+    const top = Math.round((window.innerHeight - height) / 2);
+
+    window.open('https://www.disneyplus.com/', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
+  };
+  const openSettingsPopup = () => {
+    const width = Math.round(window.innerWidth * 0.9);
+    const height = Math.round(window.innerHeight * 0.9);
+    const left = Math.round((window.innerWidth - width) / 2);
+    const top = Math.round((window.innerHeight - height) / 2);
+
+    window.open('/src/settings.jsx', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
+  };
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
       <MantineProvider
@@ -20,14 +59,15 @@ export default function App() {
           colorScheme,
         }}
       >
+        
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Text size="lg" weight={700} style={{ marginBottom: 32 }}>MediaLauncher</Text>
             <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}fontSize="12px">Button 1</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 2</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 3</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 4</Button>
+              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }} onClick={openYoutubePopup}>Youtube</Button>
+              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }} onClick={openNetflixPopup}>Netflix</Button>
+              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }} onClick={openHbomaxPopup}>Hbo max</Button>
+              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}onClick={openDisneyplusPopup}>Disney Plus</Button>
               <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 5</Button>
               <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 6</Button>
               <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 7</Button>
