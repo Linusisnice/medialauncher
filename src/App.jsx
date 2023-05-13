5a85255a029ad798301719b7d09b4a33345b8b14
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-05-13 19:55:29 +0200

    Deleted the fucking comments

--- src/App.jsx
+++ src/App.jsx
@@ -11,54 +11,7 @@ export default function App() {
       setColorScheme('light');
     }
   };
-  // const openYoutubePopup = () => {
-  //   const width = Math.round(window.innerWidth * 0.9);
-  //   const height = Math.round(window.innerHeight * 0.9);
-  //   const left = Math.round((window.innerWidth - width) / 2);
-  //   const top = Math.round((window.innerHeight - height) / 2);
 
-  //   window.open('https://www.youtube.com/', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
-  // };
-  // const openNetflixPopup = () => {
-  //   const width = Math.round(window.innerWidth * 0.9);
-  //   const height = Math.round(window.innerHeight * 0.9);
-  //   const left = Math.round((window.innerWidth - width) / 2);
-  //   const top = Math.round((window.innerHeight - height) / 2);
-
-  //   window.open('https://www.netflix.com/', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
-  // };
-  // const openHbomaxPopup = () => {
-  //   const width = Math.round(window.innerWidth * 0.9);
-  //   const height = Math.round(window.innerHeight * 0.9);
-  //   const left = Math.round((window.innerWidth - width) / 2);
-  //   const top = Math.round((window.innerHeight - height) / 2);
-
-  //   window.open('https://www.hbomax.com/', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
-  // };  
-  // const openDisneyplusPopup = () => {
-  //   const width = Math.round(window.innerWidth * 0.9);
-  //   const height = Math.round(window.innerHeight * 0.9);
-  //   const left = Math.round((window.innerWidth - width) / 2);
-  //   const top = Math.round((window.innerHeight - height) / 2);
-
-  //   window.open('https://www.disneyplus.com/', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
-  // };
-  // const openSettingsPopup = () => {
-  //   const width = Math.round(window.innerWidth * 0.9);
-  //   const height = Math.round(window.innerHeight * 0.9);
-  //   const left = Math.round((window.innerWidth - width) / 2);
-  //   const top = Math.round((window.innerHeight - height) / 2);
-
-  //   window.open('/src/settings.jsx', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
-  // };
-  // const openxboxPopup = () => {
-  //   const width = Math.round(window.innerWidth * 0.9);
-  //   const height = Math.round(window.innerHeight * 0.9);
-  //   const left = Math.round((window.innerWidth - width) / 2);
-  //   const top = Math.round((window.innerHeight - height) / 2);
-
-  //   window.open('https://www.xbox.com/play', '', `width=${width}, height=${height}, left=${left}, top=${top}`);
-  // };
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
       <MantineProvider
@@ -70,26 +23,7 @@ export default function App() {
       >
         <Notifications/>
         <AppWrap/>
-        {/* <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Text size="lg" weight={700} style={{ marginBottom: 32 }}>MediaLauncher</Text>
-            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }} onClick={openYoutubePopup}>Youtube</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }} onClick={openNetflixPopup}>Netflix</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }} onClick={openHbomaxPopup}>Hbo max</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}onClick={openDisneyplusPopup}>Disney Plus</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}onClick={openxboxPopup}>Xbox Cloud</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 6</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 7</Button>
-              <Button size="100px" radius="xl" style={{ margin: 8, borderWidth: 2, borderColor: 'transparent' }}>Button 8</Button>
-            </div>
-            <div style={{ marginTop: 32 }}>
-              <Button size="lg" radius="xl" onClick={() => toggleColorScheme()} style={{ borderWidth: 2, borderColor: 'transparent' }}>
-                Change Theme
-              </Button>
-            </div>
-          </div>
-        </div> */}
+        {}
       </MantineProvider>
     </ColorSchemeProvider>
   );
