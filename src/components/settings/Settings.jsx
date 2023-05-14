59bcbab8f6df4d413c411e6234e27d16adeadff0
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-05-14 10:41:03 +0200

    Tried to implement a slider didnt work though

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,11 +1,14 @@
 import { Drawer, Button, ActionIcon } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { TbSettings } from 'react-icons/tb';
-
+import { useState } from 'react';
+import { Slider, RangeSlider } from '@mantine/core';
 export const Settings = () => {
   const [opened, { open, close }] = useDisclosure(false);
-
+  const [value, setValue] = useState(40);
+  const [rangeValue, setRangeValue] = useState<[0, 100]>([20, 80]);
   return (
+    
     <>
       <ActionIcon color="dark" variant="filled" size={'xl'} radius={'md'} onClick={open}>
         <TbSettings size={'2rem'} />
@@ -18,6 +21,8 @@ export const Settings = () => {
         overlayProps={{ opacity: 0.5, blur: 4 }}
         size={'lg'}
       >
+        <Slider value={value} onChange={setValue} />
+        <RangeSlider value={rangeValue} onChange={setRangeValue} />
         <Button radius="xl" margin ="20 "variant="outline" fullWidth>
           Button 1
         </Button>
@@ -27,4 +32,5 @@ export const Settings = () => {
       </Drawer>
     </>
   );
+  
 };
