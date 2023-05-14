6ae6387e64b1be616565ecf9421def5c7d4067fc
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-05-14 10:00:46 +0200

    Added some buttons to the settings

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,23 +1,30 @@
-import { Drawer } from "@mantine/core";
+import { Drawer, Button, ActionIcon } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { ActionIcon } from "@mantine/core";
 import { TbSettings } from 'react-icons/tb';
 
-
 export const Settings = () => {
-
   const [opened, { open, close }] = useDisclosure(false);
 
-  return(
-      <>
-        <ActionIcon color="dark" variant="filled" size={'xl'} radius={'md'} onClick={open}>
-            <TbSettings size={'2rem'} />
-        </ActionIcon>
-          <Drawer opened={opened} onClose={close} title="Settings" position="right" overlayProps={{ opacity: 0.5, blur: 4 }} size={'lg'} >
-            Settings drawer
-          </Drawer>
-      </>
-          )
-}
-
-
+  return (
+    <>
+      <ActionIcon color="dark" variant="filled" size={'xl'} radius={'md'} onClick={open}>
+        <TbSettings size={'2rem'} />
+      </ActionIcon>
+      <Drawer
+        opened={opened}
+        onClose={close}
+        title="Settings"
+        position="right"
+        overlayProps={{ opacity: 0.5, blur: 4 }}
+        size={'lg'}
+      >
+        <Button radius="xl" margin ="20 "variant="outline" fullWidth>
+          Button 1
+        </Button>
+        <Button radius="xl" variant="outline" fullWidth>
+          Button 2
+        </Button>
+      </Drawer>
+    </>
+  );
+};
